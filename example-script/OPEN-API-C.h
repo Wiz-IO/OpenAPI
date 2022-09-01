3d07ffa19eaec71685c9074ebb06291c05ad0555
Georgi Angelov <the.wizarda@gmail.com>
2022-09-01 13:57:54 +0300

    Add files via upload

---
+++ example-script/OPEN-API-C.h
@@ -0,0 +1,27 @@
+    /*
+        OpenAPI Version: 1.0.0
+        Functions: 19    
+
+        DO NOT EDIT !!!
+
+    */
+
+    { 0x10A9DD60, analogRead },
+    { 0x1C76F7B6, micros },
+    { 0x1F55D5A2, delay },
+    { 0x20F099D4, analogWrite },
+    { 0x403B17B4, uart_close },
+    { 0x5E25265E, uart_peek },
+    { 0x6A50FC40, delayMicroseconds },
+    { 0x6A973CA4, pinMode },
+    { 0x73501778, digitalRead },
+    { 0x738E8708, uart_read },
+    { 0x73C5D0F6, getIMEI },
+    { 0x87A46DF6, api_print },
+    { 0x9EF2D668, uart_available },
+    { 0xAAC4FB6A, millis },
+    { 0xB0D9BA0E, doReset },
+    { 0xC9AE709C, digitalWrite },
+    { 0xD2361D70, getBattery },
+    { 0xD39D974C, uart_write },
+    { 0xD631CF1E, uart_open },
